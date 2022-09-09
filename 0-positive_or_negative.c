247dd7c3d8f8f38c95e3bc0950fb57826f1698ed
patpatmw <patmziya@gmail.com>
2022-09-09 15:34:37 -0700

    Positive anything is better than negative nothing

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -1,26 +1,51 @@
 #include <stdio.h>
 
-#include <unistd.h>
+#include <time.h>
 
+#include <stdlib.h>
 
-
-/*
- * main - prints exactly "and that piece of art is useful"
+/**
  *
- * - Dora Korpar, 2015-10-19",
+ *  * main - main block
  *
- *    * followed by a new line, to the standard error.
+ *   * Description: Get a random number and print the number
  *
- *     * Return: Always 0 (Success)
- */
+ *    * and if it is positive, negative, or zero
+ *
+ *     * Return: 0
+ *
+ *      */
 
 int main(void)
 
 {
 
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		int n;
 
-			return (1);
+
+
+			srand(time(0));
+
+				n = rand() - RAND_MAX / 2;
+
+
+
+					if (n > 0)
+
+								printf("%i is positive\n", n);
+
+						else if (n < 0)
+
+									printf("%i is negative\n", n);
+
+							else
+
+										printf("%i is zero\n", n);
+
+
+
+								return (0);
 
 }
+
 
