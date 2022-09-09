c731c21f4eff2cbb14510fedd660e89a54ea04ab
patpatmw <patmziya@gmail.com>
2022-09-08 19:55:14 -0700

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,14 +5,10 @@
 
 
 /**
+ * main - Entry point
+ * Return: Always 1 (Success)
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 1 (Success)
- *
- *     */
+ */
 
 int main(void)
 
