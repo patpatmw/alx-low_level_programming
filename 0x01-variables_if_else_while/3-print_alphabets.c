7c69826e554fa3a8b3eb0b15794e7fdf7a187960
patpatmw <patmziya@gmail.com>
2022-09-09 19:31:36 -0700

    alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,9 +13,9 @@ int main(void)
 int ch;
 
 for (ch = 'a'; ch <= 'z'; ch++)
-	putchar(ch);
+putchar(ch);
 for (ch = 'A'; ch <= 'Z'; ch++)
-	putchar(ch);
-	putchar('\n');
-	return (0);
+putchar(ch);
+putchar('\n');
+return (0);
 }
