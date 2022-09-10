69bf91fee9cd0b54fcd51671244d54326425cf14
patpatmw <patmziya@gmail.com>
2022-09-09 18:09:15 -0700

    print numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,16 +11,16 @@
  */
 
 int main(void)
-
 {
-int n;
-for (n = 48; n < 58; n++)
-{
+	int n;
+	
+	for (n = 48; n < 58; n++)
+	{
 	putchar(n);
+	
+	}	
 
-}
-
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 
 }
