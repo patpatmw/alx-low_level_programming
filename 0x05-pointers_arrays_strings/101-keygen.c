58fb5e2c22bae558b6c6e3c0f51f9c2fc26cca12
patpatmw <patmziya@gmail.com>
2022-09-19 18:42:21 -0700

    a function that convert a string to an integer

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,29 +11,42 @@
  * Return: generated password
  *
  */
-int main(void)
-{
-char c;
-int x;
-
-
-
-srand(time(0));
-
-while (x <= 2645)
-
 {
 
-c = rand() % 128;
+int pass[100];
 
-x += c;
+int i, sum, n;
 
-putchar(c);
 
-}
 
-putchar(2772 - x);
+sum = 0;	
 
+
+
+srand(time(NULL));
+
+
+
+for (i = 0; i < 100; i++)
+
+								{
+pass[i] = rand() % 78;
+sum += (pass[i] + '0');
+putchar(pass[i] + '0');
+
+																if ((2772 - sum) - '0' < 78)
+
+																{
+
+																n = 2772 - sum - '0';
+																sum += n;
+
+																putchar(n + '0');
+																break;
+
+																																}
+
+																}
 
 
 return (0);
