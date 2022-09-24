8aa363c6f601dacac3d063d03e4dd7cac28d6e37
patpatmw <patmziya@gmail.com>
2022-09-23 17:21:07 -0700

    a function that concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,32 +1,23 @@
-#include "main.h"
-
 /**
- *_strcat - function commute srtings
+ *_strcat - concatenates  the string pointed to by @src to
  *
- * @dest: param pointer to a char
+ * the end of the string pointed to by @dest
  *
- * @src: param pointer to a char
+ *@dest: String that will be appended
  *
- * Return: return value of dest
+ *@src: String to be concatenated upon
+ *
+ *
+ *
+ * Return: returns poiner to @dest
  *
  */
-
 char *_strcat(char *dest, char *src)
 {
-int i;
-int j;
-i = 0;
-j = 0;
-while (dest[i] != '\0')
-{
-i++;
-}
-while (src[j] != '\0')
-{
-dest[i] = src[j];
-j++;
-i++;
-}
-dest[i] = '\0';
+int index = 0, dest_len = 0;
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index]; index++)
+dest[dest_len++] = src[index];
 return (dest);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,15 +4,7 @@
 
 #include <stdio.h>
 
-/**
 
- *void prototypes()
-
- *int prototypes()
-
- *char prototypes()
-
- */
 
 void reverse_array(int *a, int n);
 
@@ -21,6 +13,8 @@ void print_number(int n);
 void print_buffer(char *b, int size);
 
 int _strcmp(char *s1, char *s2);
+
+int _putchar(char c);
 
 char *_strcat(char *dest, char *src);
 
@@ -39,6 +33,4 @@ char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 char *add_strings(char *n1, char *n2, char *r, int r_index);
-
-
-
+#endif
