104ae694e0cb8c66812755dcc8280330a7dab31b
patpatmw <patmziya@gmail.com>
2022-09-25 11:30:26 -0700

    a function that compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -24,21 +24,20 @@ if (s1[i] == '\0' && s2[i] == '\0')
 break;
 else if (s1[i] == '\0')
 {
-										diff = s2[i];
+diff = s2[i];
 break;
 }
-										else if (s2[i] == '\0')
+else if (s2[i] == '\0')
 {
 diff = s1[i];
 break;
-										}
+}
 else if (s1[i] != s2[i])
 {
 diff = s1[i] - s2[i];
 break;
 }
-else
-										i++;
-}
+else	
+i++;
 return (diff);
 }
