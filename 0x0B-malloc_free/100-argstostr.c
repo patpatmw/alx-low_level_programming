a534b3d1eab201e99bc90948e1867f0361c1fe04
patpatmw <patmziya@gmail.com>
2022-10-04 19:43:11 -0700

    additional task 1

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,54 +9,33 @@
 
 char *argstostr(int ac, char **av)
 {
-
 char *str, *s;
-
 int x, y, z, len = 0;
-
-
-
 if (ac == 0 || av == NULL)
-
 return (NULL);
-
-
-
 for (x = 0; x < ac; x++)
 {
-
 s = av[x];
-
 y = 0;
-
-
 while (s[y++])
 len++;
 len++;
 }
-
 str = (char *)malloc(sizeof(char) * (len + 1));
 if (str == NULL)
 return (NULL);
 for (x = 0, y = 0; x < ac && y < len; x++)
 {
 s = av[x];
-
 z = 0;
-
 while (s[z])
-
 {
 str[y] = s[z];
 z++;
 y++;
 }
-
 str[y++] = '\n';
-
 }
-
 str[y] = '\0';
-
 return (str);
 }
