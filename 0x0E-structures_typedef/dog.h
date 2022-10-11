1442407fc654e5b27f8e5dc3ba846940dea6b180
patpatmw <patmziya@gmail.com>
2022-10-11 00:08:20 -0700

    struck dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,28 +1,44 @@
-#ifndef _DOG_H
-#define _DOG_H
-/**
- * dog_t - Typedef for struct dog
- */
-typedef struct dog dog_t;
+#ifndef DOG_H
+
+#define DOG_H
+
+
 
 /**
-
- * struct dog - structure of a dog
-
- * @name: name of dog
-
- * @age: age of dog
-
- * @owner: owner of the dog
-
- */
+ *
+ *  * struct dog - a dog's basic info
+ *
+ *   * @name: First member
+ *
+ *    * @age: Second member
+ *
+ *     * @owner: Third member
+ *
+ *      *
+ *
+ *       * Description: Longer description
+ *
+ *        */
 
 struct dog
+
 {
+
 char *name;
 float age;
 char *owner;
+
 };
+
+
+
+/**
+ *
+ *  * dog_t - typedef for struct dog
+ *
+ *   */
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
@@ -32,6 +48,8 @@ dog_t *new_dog(char *name, float age, char *owner);
 
 void free_dog(dog_t *d);
 
+char *_strcpy(char *dest, char *src);
 
+int _strlen(char *s);
 
-#endif /*STRTYP*/
+#endif
